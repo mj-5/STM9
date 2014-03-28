93f74baad1480612b86017c901f881c24a218f19
Jazuly <achmad.zainul5@gmail.com>
2014-03-28 10:24:14 +0700

    insert passphrase - error

---
+++ src/id/stsn/stm9/Id.java
@@ -0,0 +1,10 @@
+package id.stsn.stm9;
+
+public class Id {
+
+	public static final class menu{
+		public static final class opsi{
+			public static final int buat_kunci = 0x21070001;
+		}
+	}
+}
--- src/id/stsn/stm9/ui/MasterKeyActivity.java
+++ src/id/stsn/stm9/ui/MasterKeyActivity.java
@@ -2,13 +2,41 @@ package id.stsn.stm9.ui;
 
 import id.stsn.stm9.R;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MasterKeyActivity extends Activity{
+public class MasterKeyActivity extends Activity {
 
-	public void onCreate(Bundle savedInstanceState){
+	Button btnBuatKunci;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.master_key);
+
+		btnBuatKunci = (Button) findViewById(R.id.menu_buat_kunci);
+
+		btnBuatKunci.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				buatKunci();
+			}
+		});
+
 	}
+
+	private void buatKunci() {
+		Intent intent = new Intent(this, KeyActivity.class);
+		intent.setAction(KeyActivity.BUAT_KUNCI_ACTION);
+		intent.putExtra(KeyActivity.EXTRA_GENERATE_DEFAULT_KEYS, true);
+		intent.putExtra(KeyActivity.EXTRA_USER_IDS, "");
+		startActivityForResult(intent, 0);
+
+	}
+
 }
