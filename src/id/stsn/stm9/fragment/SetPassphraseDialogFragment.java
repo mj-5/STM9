a83af961572e3532f55551cab21cd7aa475b7459
Jazuly <achmad.zainul5@gmail.com>
2014-04-15 08:44:47 +0700

    dialog fragment untuk memasukkan passphrase 2x --> ketika pertama kali membuat kunci

--- src/id/stsn/stm9/fragment/SetPassphraseDialogFragment.java
+++ src/id/stsn/stm9/fragment/SetPassphraseDialogFragment.java
@@ -12,7 +12,6 @@ import android.os.Messenger;
 import android.os.RemoteException;
 import android.support.v4.app.DialogFragment;
 
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,41 +22,35 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.TextView.OnEditorActionListener;
+import android.util.Log;
 
 public class SetPassphraseDialogFragment extends DialogFragment implements OnEditorActionListener {
     private static final String ARG_MESSENGER = "messenger";
     private static final String ARG_TITLE = "title";
-
-    public static final int MESSAGE_OKAY = 1;
-
     public static final String MESSAGE_NEW_PASSPHRASE = "new_passphrase";
 
     private Messenger mMessenger;
-    private EditText mPassphraseEditText;
-    private EditText mPassphraseAgainEditText;
+    private EditText mPassphraseEditText1;
+    private EditText mPassphraseEditText2;
+    
+    public static final int MESSAGE_OKAY = 1;
 
     /**
-     * Creates new instance of this dialog fragment
-     * 
-     * @param title
-     *            title of dialog
-     * @param messenger
-     *            to communicate back after setting the passphrase
-     * @return
+     * Dialog fragment untuk insert passphrase
      */
-    public static SetPassphraseDialogFragment newInstance(Messenger messenger, int title) {
+    public static SetPassphraseDialogFragment nInstance(Messenger messenger, int title) {
         SetPassphraseDialogFragment frag = new SetPassphraseDialogFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_TITLE, title);
-        args.putParcelable(ARG_MESSENGER, messenger);
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_TITLE, title);
+        bundle.putParcelable(ARG_MESSENGER, messenger);
 
-        frag.setArguments(args);
+        frag.setArguments(bundle);
 
         return frag;
     }
 
     /**
-     * Creates dialog
+     * membuat dialog
      */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -69,51 +62,57 @@ public class SetPassphraseDialogFragment extends DialogFragment implements OnEdi
         AlertDialog.Builder alert = new AlertDialog.Builder(activity);
 
         alert.setTitle(title);
-        alert.setMessage(R.string.dua_kali_passphrase);
+        alert.setMessage(R.string.enter_passphrase_twice);
 
         LayoutInflater inflater = activity.getLayoutInflater();
-        View view = inflater.inflate(R.layout.dua_passphrase, null);
+        View view = inflater.inflate(R.layout.passphrase_ulangi, null);
         alert.setView(view);
 
-        mPassphraseEditText = (EditText) view.findViewById(R.id.passphrase_passphrase);
-        mPassphraseAgainEditText = (EditText) view.findViewById(R.id.passphrase_passphrase_again);
+        mPassphraseEditText1 = (EditText) view.findViewById(R.id.passphrase_once);
+        mPassphraseEditText2 = (EditText) view.findViewById(R.id.passphrase_twice);
 
+        // OK button
         alert.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             
             @Override
             public void onClick(DialogInterface dialog, int id) {
                 dismiss();
 
-                String passPhrase1 = mPassphraseEditText.getText().toString();
-                String passPhrase2 = mPassphraseAgainEditText.getText().toString();
-                if (!passPhrase1.equals(passPhrase2)) {
+                String passphraseOnce = mPassphraseEditText1.getText().toString();
+                String passphraseTwice = mPassphraseEditText2.getText().toString();
+                
+                // if passphrase not same when input
+                if (!passphraseOnce.equals(passphraseTwice)) {
                     Toast.makeText(
                             activity,
-                            getString(R.string.pesan_error,
-                                    getString(R.string.passphrases_tidak_cocok)), Toast.LENGTH_SHORT)
+                            getString(R.string.error_message,
+                                    getString(R.string.passphrases_do_not_match)), Toast.LENGTH_SHORT)
                             .show();
                     return;
                 }
 
-                if (passPhrase1.equals("")) {
+                // if passphrase empty
+                if (passphraseOnce.equals("")) {
                     Toast.makeText(
                             activity,
-                            getString(R.string.pesan_error,
-                                    getString(R.string.passphrases_harus_isi)),
+                            getString(R.string.error_message,
+                                    getString(R.string.passphrase_must_not_be_empty)),
                             Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 // return resulting data back to activity
                 Bundle data = new Bundle();
-                data.putString(MESSAGE_NEW_PASSPHRASE, passPhrase1);
+                data.putString(MESSAGE_NEW_PASSPHRASE, passphraseOnce);
 
                 sendMessageToHandler(MESSAGE_OKAY, data);
             }
         });
 
+        // Cancel button
         alert.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
             
+        	// back to KeyActivity
             @Override
             public void onClick(DialogInterface dialog, int id) {
                 dismiss();
@@ -128,22 +127,22 @@ public class SetPassphraseDialogFragment extends DialogFragment implements OnEdi
         super.onActivityCreated(arg0);
 
         // request focus and open soft keyboard
-        mPassphraseEditText.requestFocus();
+        mPassphraseEditText1.requestFocus();
         getDialog().getWindow().setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
-        mPassphraseAgainEditText.setOnEditorActionListener(this);
+        mPassphraseEditText2.setOnEditorActionListener(this);
     }
 
     /**
-     * Associate the "done" button on the soft keyboard with the okay button in the view
+     * Associate "done" button on the soft keyboard with the okay button in the view
      */
     @Override
     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
         if (EditorInfo.IME_ACTION_DONE == actionId) {
             AlertDialog dialog = ((AlertDialog) getDialog());
-            Button bt = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+            Button button = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
 
-            bt.performClick();
+            button.performClick();
             return true;
         }
         return false;
@@ -151,9 +150,6 @@ public class SetPassphraseDialogFragment extends DialogFragment implements OnEdi
 
     /**
      * Send message back to handler which is initialized in a activity
-     * 
-     * @param what
-     *            Message integer you want to send
      */
     private void sendMessageToHandler(Integer what, Bundle data) {
         Message msg = Message.obtain();
