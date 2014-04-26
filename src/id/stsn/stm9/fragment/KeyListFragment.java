03cf8e27a108c67f94e74d22fbb0b644e4da2cbb
Jazuly <achmad.zainul5@gmail.com>
2014-04-26 18:54:43 +0700

    Key list fragment - show public keys

--- src/id/stsn/stm9/fragment/KeyListFragment.java
+++ src/id/stsn/stm9/fragment/KeyListFragment.java
@@ -1,31 +1,50 @@
 package id.stsn.stm9.fragment;
 
-import id.stsn.stm9.R;
-import id.stsn.stm9.activity.ListKeyActivity;
-import id.stsn.stm9.provider.ProviderHelper;
 import id.stsn.stm9.Id;
+import id.stsn.stm9.R;
+import id.stsn.stm9.activity.KeyListActivity;
+import id.stsn.stm9.activity.KeyServerUploadActivity;
+import id.stsn.stm9.provider.KeyContract.KeyRings;
+import id.stsn.stm9.provider.KeyContract.UserIds;
+import id.stsn.stm9.services.KeyListAdapter;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.app.LoaderManager;
 import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.ExpandableListContextMenuInfo;
 
-public class KeyListFragment extends ExpandableListFragment {
-    protected ListKeyActivity mKeyListActivity;
+public class KeyListFragment extends ListKeyFragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
+    private KeyListActivity mKeyListPublicActivity;
+
+    private KeyListAdapter mAdapter;
+
+    /**
+     * Define Adapter and Loader on create of Activity
+     */
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        mKeyListActivity = (ListKeyActivity) getActivity();
+        mKeyListPublicActivity = (KeyListActivity) getActivity();
 
-        // register long press context menu
-        registerForContextMenu(getListView());
+        mAdapter = new KeyListAdapter(mKeyListPublicActivity, null, Id.tipe.public_key);
+        setListAdapter(mAdapter);
 
-        // Give some text to display if there is no data. In a real application this would come from a resource.
-        setEmptyText(getString(R.string.list_empty));
+        // Start out with a progress indicator.
+        setListShown(false);
+
+        // Prepare the loader. Either re-connect with an existing one, or start a new one.
+        // id is -1 as the child cursors are numbered 0,...,n
+        getLoaderManager().initLoader(-1, null, this);
     }
 
     /**
@@ -34,32 +53,61 @@ public class KeyListFragment extends ExpandableListFragment {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        menu.add(0, Id.menu.export, 5, R.string.menu_export_key);
+        menu.add(0, Id.menu.exportToServer, 2, R.string.menu_export_key_to_server);
     }
 
     @Override
     public boolean onContextItemSelected(android.view.MenuItem item) {
         ExpandableListContextMenuInfo expInfo = (ExpandableListContextMenuInfo) item.getMenuInfo();
 
-        // expInfo.id would also return row id of childs, but we always want to get the row id of
-        // the group item, thus we are using the following way
+        // expInfo.id would also return row id of childs, but we always want to get the row id of the group item, 
+        // thus we are using the following way
         int groupPosition = ExpandableListView.getPackedPositionGroup(expInfo.packedPosition);
         long keyRingRowId = getExpandableListAdapter().getGroupId(groupPosition);
 
         switch (item.getItemId()) {
-        case Id.menu.export:
-            long masterKeyId = ProviderHelper.getPublicMasterKeyId(mKeyListActivity, keyRingRowId);
-            if (masterKeyId == -1) {
-                masterKeyId = ProviderHelper.getSecretMasterKeyId(mKeyListActivity, keyRingRowId);
-            }
+        case Id.menu.exportToServer:
+        	Intent uploadIntent = new Intent(mKeyListActivity, KeyServerUploadActivity.class);
+        	uploadIntent.setAction(KeyServerUploadActivity.ACTION_EXPORT_KEY_TO_SERVER);
+        	uploadIntent.putExtra(KeyServerUploadActivity.EXTRA_KEYRING_ROW_ID, (int)keyRingRowId);
+        	startActivityForResult(uploadIntent, Id.request.export_to_server);
 
-            mKeyListActivity.showExportKeysDialog(masterKeyId);
-            return true;
-
+        	return true;
+        	
         default:
             return super.onContextItemSelected(item);
 
         }
+    }
+
+    // These are the rows that we will retrieve.
+    static final String[] PROJECTION = new String[] { KeyRings._ID, KeyRings.MASTER_KEY_ID,
+            UserIds.USER_ID };
+
+    static final String SORT_ORDER = UserIds.USER_ID + " ASC";
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri baseUri = KeyRings.buildPublicKeyRingsUri();
+
+        return new CursorLoader(getActivity(), baseUri, PROJECTION, null, null, SORT_ORDER);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mAdapter.setGroupCursor(data);
+
+        // The list should now be shown.
+        if (isResumed()) {
+            setListShown(true);
+        } else {
+            setListShownNoAnimation(true);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.setGroupCursor(null);
     }
 
 }
